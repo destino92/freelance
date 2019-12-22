ea9654c0b8efe67cb12d5a4b80329cf57a529756
ruky-a <rukevwe2006@gmail.com>
2019-12-22 13:09:05 -0500

    User show-page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'users/dashboard'
   get '/dashboard', to: 'users#dashboard'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'users'
   get '/selling_orders', to: 'orders#selling_orders'
    get '/buying_orders', to: 'orders#buying_orders'
    get '/all_requests', to: 'requests#list'
