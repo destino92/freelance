697a582ab1c7d98d6d3e042e4c746cd16b569cf9
ruky-a <rukevwe2006@gmail.com>
2019-12-23 19:42:29 -0500

    Remove Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :omniauthable
+         :recoverable, :rememberable, :validatable 
+         # :omniauthable
 
          validates :full_name, presence: true, length: {maximum: 50}
 
