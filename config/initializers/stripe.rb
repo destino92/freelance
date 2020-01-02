340c33d197bdaa02f6023160c46146eb1077bb75
ruky-a <rukevwe2006@gmail.com>
2020-01-02 04:58:49 -0500

    home

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,8 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['PUBLISHABLE_KEY'],
   secret_key: ENV['SECRET_KEY']
+
 }
 
-# Stripe.api_key = 'sk_test_GlRlt1FyFGXPaPjMyiXhPvmY00qm876Zh3'
+
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
