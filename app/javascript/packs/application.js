bb09104d142bd342cf9fd1300d89ddaecca7f229
ruky-a <rukevwe2006@gmail.com>
2020-03-30 10:41:57 -0400

    Update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,6 +33,8 @@ $(document).on('turbolinks:load',() => {
 })
 
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
