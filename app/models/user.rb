f86503b105c39f6eb1e2ed16e703648ef0d556e6
Destino Dello <destinodello@gmail.com>
2020-06-09 16:24:35 +0000

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,45 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
+         :recoverable, :rememberable, :validatable, 
          # :omniauthable
+         :omniauthable, :omniauth_providers => [:facebook]
 
          validates :full_name, presence: true, length: {maximum: 50}
 
          has_one_attached :avatar
 
-        has_many :gigs
-        has_many :requests
+         has_many :gigs
+         has_many :requests
          has_many :buying_orders, foreign_key: "buyer_id", class_name: "Order"
-        has_many :selling_orders, foreign_key: "seller_id", class_name: "Order"
-        has_many :offers
+         has_many :selling_orders, foreign_key: "seller_id", class_name: "Order"
+         has_many :offers
+
+
+        def self.new_with_session(params, session)
+            super.tap do |user|
+                if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+                    user.email = data["email"] if user.email.blank?
+                end
+            end
+        end
+
+        def self.from_omniauth(auth)
+            user = User.where(email: auth.info.email).first
+
+            if user
+                return user
+            else
+                where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+                    user.email = auth.info.email
+                    user.password = Devise.friendly_token[0,20]
+                    user.name = auth.info.name   # assuming the user model has a name
+                    user.image = auth.info.image # assuming the user model has an image
+                    user.uid = auth.uid
+                    user.provider = auth.provider
+                end
+            end
+        end
 
 
 #   def self.from_omniauth(auth)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'message/create'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'pages#home'
   get 'users/dashboard'
   get '/dashboard', to: 'users#dashboard'
