e09a6b9dd23433eeb85e905a309572ba1b58ddda
Destino Dello <destinodello@gmail.com>
2020-11-16 04:12:16 +0000

    initial cloudinary dropbox sample

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("raty-js")
 
 
 window.Noty = require("noty")
-window.Dropzone = require("dropzone")
 window.BulmaCarousel = require("bulma-extensions/bulma-carousel/dist/js/bulma-carousel")
 
 window.Calendar = require("@fullcalendar/core").Calendar;
@@ -41,69 +40,4 @@ $(document).on('turbolinks:load',() => {
 require("trix")
 require("@rails/actiontext")
 
-const Uppy = require('@uppy/core')
-const Dashboard = require('@uppy/dashboard')
-const ActiveStorageUpload = require('@excid3/uppy-activestorage-upload')
-
-require('@uppy/core/dist/style.css')
-require('@uppy/dashboard/dist/style.css')
-
-document.addEventListener('turbolinks:load', () => {
-  document.querySelectorAll('[data-uppy]').forEach(element => setupUppy(element))
-})
-
-function setupUppy(element) {
-  let trigger = element.querySelector('[data-behavior="uppy-trigger"]')
-  let form = element.closest('form')
-  let direct_upload_url = document.querySelector("meta[name='direct-upload-url']").getAttribute("content")
-  let field_name = element.dataset.uppy
-
-  trigger.addEventListener("click", (event) => event.preventDefault())
-
-  let uppy = Uppy({
-    autoProceed: false,
-    allowMultipleUploads: true,
-    logger: Uppy.debugLogger
-  })
-
-  uppy.use(ActiveStorageUpload, {
-    directUploadUrl: direct_upload_url
-  })
-
-  uppy.use(Dashboard, {
-    trigger: trigger,
-    closeAfterFinish: false,
-  })
-
-  uppy.on('complete', (result) => {
-    // Rails.ajax
-    // or show a preview:
-    element.querySelectorAll('[data-pending-upload]').forEach(element => element.parentNode.removeChild(element))
-
-    result.successful.forEach(file => {
-      appendUploadedFile(element, file, field_name)
-      setPreview(element, file)
-    })
-
-    uppy.reset()
-  })
-}
-
-function appendUploadedFile(element, file, field_name) {
-  const hiddenField = document.createElement('input')
-
-  hiddenField.setAttribute('type', 'hidden')
-  hiddenField.setAttribute('name', field_name)
-  hiddenField.setAttribute('data-pending-upload', true)
-  hiddenField.setAttribute('value', file.response.signed_id)
-
-  element.appendChild(hiddenField)
-}
-
-function setPreview(element, file) {
-  let preview = element.querySelector('[data-behavior="uppy-preview"]')
-  if (preview) {
-    let src = (file.preview) ? file.preview : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSpj0DBTVsaja01_xWh37bcutvpd7rh7zEd528HD0d_l6A73osY"
-    preview.src = src
-  }
-}
+import "controllers"
