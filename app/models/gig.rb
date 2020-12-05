66192799d318d6516e54aaec3935be753e860029
Destino Dello <destinodello@gmail.com>
2020-12-05 01:48:20 +0000

    added algolia config to gigs

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -13,8 +13,39 @@ class Gig < ApplicationRecord
   validates :title, presence: {message: 'Ne peut pas etre vide'}
   validates :price, presence: {message: 'Ne peut pas etre vide'}
 
-  algoliasearch do
-    # Use all default configuration
+  algoliasearch per_environment: true do
+    attribute :title, :description, :price, :created_at, :created_at_i
+
+    attribute :category do
+      { name: category.name }
+    end
+
+    attribute :user do
+      { full_name: user.full_name }
+    end
+
+    attribute :average_rating do
+        reviews.count == 0 ? 0 : reviews.average(:stars).round(1)
+    end
+
+    attribute :created_at_i do
+      created_at.to_i
+    end
+
+    attribute :photo do
+      photos.first.url
+    end
+
+    searchableAttributes ['unordered(title)', 'unordered(description)', 'unordered(user)']
+
+    customRanking ['desc(avg_rating)']
+
+    add_replica "Item_#{Rails.env}_sort_date", inherit: true do # backward compatibility naming
+      customRanking ['desc(created_at_i)']
+      ranking ['custom']
+    end
+
+    attributesForFaceting ['searchable(category)']
   end
 
   def average_rating
